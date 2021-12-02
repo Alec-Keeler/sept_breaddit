04dfe7594a793aee8838ae219ae6b693d346c08d
Alec Keeler <alec.keeler@gmail.com>
2021-12-02 12:10:02 -0600

    more pug :)

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const app = express()
+const app = express();
+const { User } = require('./models');
 
 app.set('view engine', 'pug')
 
 // path: / method: get
 app.get('/', (req, res) => {
     // res.send('You have arrived at Breaddit'
-    res.render('home', {title: 'Home Page', class: 'banana'})
+    res.render('home', {title: 'Home Page', fruit: 'banana'})
+})
+
+app.get('/users', async(req, res) => {
+    const users = await User.findAll()
+    // console.log(users)
+    res.render('users', {title: 'Users List', users})
 })
 
 
