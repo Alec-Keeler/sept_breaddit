425a2d42d19f326a60bb46e30c1a85fd25f75030
Alec Keeler <alec.keeler@gmail.com>
2021-12-03 15:05:58 -0600

    W11D3

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const { User } = require('./models');
 const userRouter = require('./routes/users');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'pug')
+app.use(express.static('public'))
+// middleware to access form data vvvv
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+    req.banana = 'banana'
+    next()
+})
 
 app.use('/users', userRouter)
 app.use('/banana', userRouter)
@@ -14,18 +24,6 @@ app.get('/', (req, res) => {
     res.render('home', {title: 'Home Page', fruit: 'banana'})
 })
 
-
-// app.get('/users', async(req, res) => {
-//     const users = await User.findAll()
-//     // console.log(users)
-//     res.render('users', {title: 'Users List', users})
-// })
-
-// // method: get path: /users/:id
-// app.get('/users/:id(\\d+)', async(req, res) => {
-//     const user = await User.findByPk(req.params.id)
-//     res.render('profile', {title: 'Profile Page', user})
-// })
 
 // /about/, /foo/, /about-foo/, /about_foo/
 // ! /about/foo, /something/somethingelse, /word/word/word
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
+const csurf = require('csurf')
+const csrfProtection = csurf({ cookie: true })
 
 router.use((req, res, next) => {
     console.log('hello from users router', req.path)
     next()
 })
 
+const testBanana = (req, res, next) => {
+    console.log(req.banana)
+    next()
+}
+
 // /users/
-router.get('/users', async (req, res) => {
+router.get('/', testBanana, async (req, res) => {
     const users = await User.findAll()
-    // console.log(users)
-    res.render('users', { title: 'Users List', users })
+    if (req.banana) {
+        res.render('users', { title: 'Users List', users })
+    } else {
+        res.send('where is the banana')
+    }
 })
 
 // method: get path: /users/:id
-router.get('/users/:id(\\d+)', async (req, res) => {
+router.get('/:id(\\d+)', async (req, res, next) => {
     const user = await User.findByPk(req.params.id)
     res.render('profile', { title: 'Profile Page', user })
+})
+
+router.get('/signup', csrfProtection, (req, res) => {
+    res.render('signup', { csrfToken: req.csrfToken(), errors: [], data: {} })
+})
+
+const emailChecker = (req, res, next) => {
+    const exp = /[\w\d-]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/
+    req.errors = []
+    if (exp.test(req.body.email)) {
+        next()
+    } else {
+        req.errors.push('Please provide a valid email')
+        next()
+    }
+}
+
+
+router.post('/signup', emailChecker, csrfProtection, async(req, res) => {
+    console.log(req.body)
+    const { username, email, password } = req.body
+    if (req.errors.length === 0) {
+        const user = await User.create({
+            username,
+            email,
+            password
+        })
+        res.redirect('/users')
+    } else {
+        console.log(req.errors)
+        const errors = req.errors
+        res.render('signup', {csrfToken: req.csrfToken(), errors, data: req.body})
+    }
 })
 
 
