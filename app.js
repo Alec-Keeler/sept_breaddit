c772113cf045673b1626bfb4d31bbaa0ae5cdc1a
Alec Keeler <alec.keeler@gmail.com>
2021-12-09 11:38:53 -0600

    w12d3

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use((req, res, next) => {
-    console.log('session: ', req.session)
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log('session: ', req.session)
+//     next()
+// })
 
 app.use((req, res, next) => {
     if (req.session.user) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,19 +1,46 @@
 const express = require('express')
 const router = express.Router()
+const csurf = require('csurf')
+const csrfProtection = csurf({ cookie: true })
+const { Post } = require('../db/models')
 
-router.use((req, res, next) => {
+const authCheck = (req, res, next) => {
     if (req.session.user) {
         next()
     } else {
-        res.redirect('/users/signup')
+        res.redirect('/users/login')
+    }
+}
+
+
+router.get('/', csrfProtection, async(req, res) => {
+    const posts = await Post.findAll()
+    res.render('posts', {posts, csrfToken: req.csrfToken()})
+})
+
+router.get('/new', authCheck, csrfProtection, (req, res) => {
+    res.render('new-post', {csrfToken: req.csrfToken()})
+})
+
+router.post('/new', csrfProtection, async(req, res) => {
+    const {title, content} = req.body
+    const post = await Post.create({
+        title,
+        content,
+        subId: 1,
+        userId: req.session.user.userId
+    })
+    res.redirect('/posts')
+})
+
+router.post('/:id/delete', csrfProtection, async(req, res) => {
+    const post = await Post.findByPk(req.params.id)
+    if (post) {
+        await post.destroy();
+        res.redirect('/posts')
+    } else {
+        res.send('what the heck there was an error')
     }
 })
-
-
-router.get('/', (req, res) => {
-    res.send('you have arrived at the posts router')
-})
-
-
 
 module.exports = router;
